4a9d810a54b899af0a3adf495ad71ecec5375c58
calico32 <devdoge1@gmail.com>
2023-03-23 23:28:05 -0400

    update packages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ class CustomDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
             rel="stylesheet"
